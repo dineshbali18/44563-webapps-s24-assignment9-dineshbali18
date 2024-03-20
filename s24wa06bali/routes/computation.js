42442156039331052277867b09aaad8b745c5589
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2024-03-20 18:05:46 -0500

    minor fix in computation

--- s24wa06bali/routes/computation.js
+++ s24wa06bali/routes/computation.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   const x = req.query.x ? parseFloat(req.query.x) : randomNumber;
-  res.send(`Math.hypot() of ${x} is ${Math.hypot(x)}<br> Math.ceil() applied to ${x} is ${Math.ceil(x)}<br> Math.clz32() applied to ${x} is ${Math.clz32(x)}`);
+  res.send(`Math.hypot() applied to ${x} is ${Math.hypot(x)}<br> Math.ceil() applied to ${x} is ${Math.ceil(x)}<br> Math.clz32() applied to ${x} is ${Math.clz32(x)}`);
 });
 
 module.exports = router;
