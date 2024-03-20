35a3a5644f4b40e45d5d3b2de18cca6f6c1047c4
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2024-03-20 15:10:50 -0500

    implemented assignment 9

---
+++ s24wa06bali/routes/computation.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* Computation end point */
+router.get('/', function(req, res, next) {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const x = req.query.x ? parseFloat(req.query.x) : randomNumber;
+  res.send(`Math.hypot() of ${x} is ${Math.hypot(x)}<br> Math.ceil() applied to ${x} is ${Math.ceil(x)}<br> Math.clz32() applied to ${x} is ${Math.clz32(x)}`);
+});
+
+module.exports = router;
--- s24wa06bali/routes/mydata.js
+++ s24wa06bali/routes/mydata.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var product=1;
-var factor=1;
-
-/* GET users listing. */
+/* GET my data. */
 router.get('/', function(req, res, next) {
-  factor++;
-  final_res=product*factor
-  res.send(`Product is: ${final_res}`);
+  res.render('mydata', { fullName: 'Dinesh Bali' });
 });
 
 module.exports = router;
